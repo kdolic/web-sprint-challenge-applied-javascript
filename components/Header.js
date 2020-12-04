ae62895f927451cafa702d3b12c8d9e5c1b3bb77
Kenan Dolic <kdolic@alumni.iastate.edu>
2020-12-04 10:31:12 -0600

    Header Component

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    // Create Elements
+    const headerDiv = document.createElement('div');
+    const date = document.createElement('span');
+    const header = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // Add Classes
+    headerDiv.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+
+    // Add textContent
+    date.textContent = 'MARCH 28, 2020';
+    header.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    // Append
+    headerDiv.appendChild(date);
+    headerDiv.appendChild(header);
+    headerDiv.appendChild(temp);
+
+    // Return
+    return headerDiv
+
+}
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
